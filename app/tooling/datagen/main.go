673b86688c9825bcf9c5279c47fa99f5df171145
Thomas Renken <t.renken@ewe.de>
2023-03-07 23:46:49 +0100

    generate data concurrency

--- app/tooling/datagen/main.go
+++ app/tooling/datagen/main.go
@@ -11,16 +11,34 @@ import (
 
 func main() {
 	now := time.Now()
+	num := 10
 
 	var billingAccounts []data.BillingAccount
-	for i := 0; i < 1; i++ {
-		account := data.BillingAccount{}
+	accountChannel := make(chan data.BillingAccount)
+
+	account := data.BillingAccount{}
+	for i := 0; i < num; i++ {
+		go func() {
+			err := faker.FakeData(&account)
+			if err != nil {
+				fmt.Println(err)
+			}
+			accountChannel <- account
+		}()
+	}
+
+	for i := 0; i < num; i++ {
+		r := <-accountChannel
+		billingAccounts = append(billingAccounts, r)
+	}
+
+	/*	for i := 0; i < 1; i++ {
 		err := faker.FakeData(&account)
 		if err != nil {
 			fmt.Println(err)
 		}
 		billingAccounts = append(billingAccounts, account)
-	}
+	}*/
 
 	indent, _ := json.MarshalIndent(&billingAccounts, "", "\t")
 	fmt.Println(string(indent))
